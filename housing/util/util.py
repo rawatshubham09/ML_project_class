c6e254f63d97db94b38218f60c810d82d51f53f0
Shubham Rawat <rawatshbham09@gmail.com>
2022-07-03 08:43:20 +0530

    error fixed and tested

--- housing/util/util.py
+++ housing/util/util.py
@@ -1,13 +1,15 @@
 import yaml
 from housing.exception import HousingException
+import yaml
+from housing.exception import HousingException
 import os,sys
 
-def read_yaml_file(file_path:str) -> dict:
+def read_yaml_file(file_path:str)->dict:
     """
-    Read a YAML file and returns the contents as a dictionary.
+    Reads a YAML file and returns the contents as a dictionary.
     file_path: str
     """
-    try :
+    try:
         with open(file_path, 'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
     except Exception as e:
